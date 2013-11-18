228a2b53bdec2c4d4dc722b39081b1bce50a7cb6
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2013-11-18 20:01:07 +0100

    use GameBase of gamedev_helpers and got rid of the boilerplate code

--- web/ld27.dart
+++ web/ld27.dart
@@ -3,83 +3,60 @@ library ld27;
 import 'package:ld27/client.dart';
 
 void main() {
-  var canvas = querySelector('canvas');
-  canvas.width = MAX_WIDTH;
-  canvas.height = MAX_HEIGHT;
-  var helper = new GameHelper('ld27');
-
-  Future.wait([helper.loadSpritesheet('assets'), helper.loadPolygons('assets')]).then((result) {
-    Game game = new Game(canvas, helper, new LayeredSpriteSheet(result[0]), result[1]);
-    game.init();
-
-    window.requestAnimationFrame(game.update);
-  });
+  new Game().start();
 }
 
-class Game {
-  CanvasElement canvas;
+class Game extends GameBase {
   LayeredSpriteSheet sheet;
-  Map<String, List<Polygon>> bodyDefs;
-  GameHelper helper;
 
-  World world = new World();
+  Game() : super('ld27', 'canvas', MAX_WIDTH, MAX_HEIGHT);
 
-  Game(this.canvas, this.helper, this.sheet, this.bodyDefs);
-
-  void init() {
-    bodyDefs.forEach((bodyId, shapes) {
-      var offset = sheet.getLayerFor('$bodyId.png').sprites['$bodyId.png'].offset;
-      shapes.forEach((shape) {
-        shape.vertices = shape.vertices.map((vertex) => vertex + offset).toList();
-      });
-    });
-
-    var tm = new TagManager();
-    world.addManager(tm);
+  void onInit() {
+    sheet = new LayeredSpriteSheet(spriteSheet);
+    world.addManager(new TagManager());
     world.addManager(new PlayerManager());
+  }
 
+  void onInitDone() {}
+
+  void createEntities() {
     var entity = addNewEntity(world, [new Transform.w2d(MAX_WIDTH/2.0, MAX_HEIGHT - 100.0, 0.0), new Velocity(0.0, 0.0), new BodyDef('ship_0')]);
     addNewEntity(world, [new Transform.w2d(MAX_WIDTH/2.0, MAX_HEIGHT - 170.0, 0.0), new Velocity(0.0, 0.0), new Gun(1, 500.0)], player: PLAYER_1);
 
-    tm.register(entity, PLAYER_1);
+    (world.getManager(TagManager) as TagManager).register(entity, PLAYER_1);
+  }
 
-    // Input
-    world.addSystem(new MouseInputHandlingSystem(canvas));
-    // Logic
-    world.addSystem(new MovementSystem());
-    world.addSystem(new CollisionDetectionSystem(bodyDefs));
-    world.addSystem(new DestroyOnCollisionSystem());
-    world.addSystem(new DestructionSystem());
-    world.addSystem(new DamageToHealthSystem());
-    world.addSystem(new CooldownSystem());
-    world.addSystem(new ExpirationSystem());
-    // Spawning
-    world.addSystem(new CollisionImpactSpawningSystem());
-    world.addSystem(new DestructionExplosionSpawningSystem());
-    world.addSystem(new BulletSpawningSystem());
-    world.addSystem(new TruckSpawningSystem());
-    world.addSystem(new TankSpawningSystem());
-    world.addSystem(new PlaneSpawningSystem());
-    world.addSystem(new FeatureActivationSystem(_achievementLoader, _graphicsLoader, sheet, helper.audioHelper));
-    // Sound
-    world.addSystem(new SoundSystem(helper.audioHelper));
-    // Rendering
-    world.addSystem(new BackgroundRenderingSystem(canvas.context2D, sheet));
-    world.addSystem(new EntityRenderingSystem(canvas.context2D, sheet));
-    world.addSystem(new AchievementRenderSystem(canvas.context2D));
-//    world.addSystem(new DebugBodyDefRenderingSystem(canvas.context2D, bodyDefs));
-
-    world.initialize();
-    world.delta = 16.66;
+  List<EntitySystem> getSystems() {
+    return [
+      // Input
+      new MouseInputHandlingSystem(canvas),
+      // Logic
+      new MovementSystem(),
+      new CollisionDetectionSystem(bodyDefs),
+      new DestroyOnCollisionSystem(),
+      new DestructionSystem(),
+      new DamageToHealthSystem(),
+      new CooldownSystem(),
+      new ExpirationSystem(),
+      // Spawning
+      new CollisionImpactSpawningSystem(),
+      new DestructionExplosionSpawningSystem(),
+      new BulletSpawningSystem(),
+      new TruckSpawningSystem(),
+      new TankSpawningSystem(),
+      new PlaneSpawningSystem(),
+      new FeatureActivationSystem(_achievementLoader, _graphicsLoader, sheet, helper.audioHelper),
+      // Sound
+      new SoundSystem(helper.audioHelper),
+      // Rendering
+      new BackgroundRenderingSystem(canvas.context2D, sheet),
+      new EntityRenderingSystem(canvas.context2D, sheet),
+      new AchievementRenderSystem(canvas.context2D),
+     ];
   }
 
   Future<Map<String, String>> _achievementLoader() => helper.loadAchievements().then(setAchievementsToAchivementRenderSystem);
   Future<SpriteSheet> _graphicsLoader(int version) => helper.loadSpritesheet('assetsv$version');
-
-  void update(_) {
-    world.process();
-    window.requestAnimationFrame(update);
-  }
 
   void setAchievementsToAchivementRenderSystem(Map<String, String> achievements) {
     AchievementRenderSystem system = world.getSystem(AchievementRenderSystem);
